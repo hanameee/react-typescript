0f22d3be8defb656f860832e6e1ff54a7b5da562
hanameee <hanameee@gmail.com>
2020-01-21 02:14:08 +0900

    Add ReducerSample that has complex state object
    ReducerSample has various state with different types.
    By using typescript, State type and Action type is added to prevent mistake.
    Can also take advantage of autocomplete.

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import Counter from "./Counter";
+import ReducerSample from "./ReducerSample";
 const App: React.FC = () => {
     return (
         <div className="App">
-            {/* <MyForm onSubmit={onSubmit} /> */}
-            <Counter />
+            <ReducerSample />
         </div>
     );
 };
