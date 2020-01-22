edce7ee375d627481a2446783044655e1655c6a1
hanameee <hanameee@gmail.com>
2020-01-22 16:44:16 +0900

    Create useTodos custom hook
    useTodos custom hook is used to get todos state from RootState.
    TodoList gets the todos state using this hook and sends down each todo TodoItem component.

--- ts-react-redux-tutorial/src/components/TodoList.tsx
+++ ts-react-redux-tutorial/src/components/TodoList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Todo } from "../modules/todos";
 import TodoItem from "./TodoItem";
+import useTodos from "../hooks/useTodos";
 
 function TodoList() {
-    const todos: Todo[] = []; // TODO: custom hook 사용해서 조회
+    const todos: Todo[] = useTodos(); // TODO: custom hook 사용해서 조회
     if (todos.length === 0) return <p>등록된 할일이 없습니다</p>;
     return (
         <ul>
